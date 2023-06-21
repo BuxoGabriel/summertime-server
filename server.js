6f3ea727c9e3c5660cee31158958fa71aea2d469
Gabriel <gabrielbuxo1@gmail.com>
2023-06-21 05:21:26 +0200

    I think the .. was an illegal move from project root in docker container. Hopefully with this change the program deploys and can find 'funcs'

--- server.js
+++ server.js
@@ -19,12 +19,12 @@ app.listen(PORT, () => {
     console.log("Summertime Server running on port " + PORT)
 })
 
-const initSoundSocket = require("../summertime-server/funcs/soundSocket")
+const initSoundSocket = require("./funcs/soundSocket")
 const soundSocketServer = http.createServer().listen(soundSocketPort, () => console.log("soundServer listening on port " + soundSocketPort))
 const soundSocket = new Server(soundSocketServer, serverOptions)
 initSoundSocket(soundSocket)
 
-const initChatSocket = require("../summertime-server/funcs/chatSocket")
+const initChatSocket = require("./funcs/chatSocket")
 const chatRoomServer = http.createServer().listen(chatRoomPort, () => console.log("chatServer listening on port " + chatRoomPort))
 const chatSocket = new Server(chatRoomServer, serverOptions)
 initChatSocket(chatSocket)
